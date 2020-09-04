6be05d7089097645e42dda4d721b42ea67e945f5
cappaul <70642710+cappaul@users.noreply.github.com>
2020-09-04 12:06:31 +0200

    moved tabs to components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./../styles.css";
 
-const HomePage = () => {
+const HomePage = (props) => {
   const [myHookContent, setMyHookContent] = useState("yay hooks");
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello {props.Label}</h1>
       <h2>Start editing to see some magic happen!</h2>
       {myHookContent}
       <br />
