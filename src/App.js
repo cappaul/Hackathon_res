cb96c89ce797bda0eda453fb5181dd5b0c9b2c85
cappaul <70642710+cappaul@users.noreply.github.com>
2020-09-08 09:24:01 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import SimpleTabs from "./components/Tabs";
 import HomePage from "./pages/HomePage";
-
+import EinoPage from "./pages/EinoPage";
 // { useState }
 
 const App = () => {
   return (
     <SimpleTabs>
-      <HomePage Label={"the first page"} />
-      <HomePage Label={"the second page"} />
+      <HomePage Label={"the Eino page"} />
+      <EinoPage Label={"other page"} />
+      <HomePage Label={"the TEAM page"} />
       <HomePage Label={"the n'th page"} />
       <HomePage Label={"the infinite page"} />
     </SimpleTabs>
---
+++ src/pages/EinoPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const EinoPage = () => <div>Ecalculatorino</div>;
+
+export default EinoPage;
